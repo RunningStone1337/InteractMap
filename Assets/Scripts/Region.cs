ded565282968d7137de5dce166a37326382aee13
RunningStone1337 <runningstone@yandex.ru>
2022-04-28 00:32:27 +0300

    Map regions completed

--- Assets/Scripts/Region.cs
+++ Assets/Scripts/Region.cs
@@ -1,15 +1,24 @@
-using Scene;
 using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Region : MonoBehaviour, IPointerClickHandler
+public class Region : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public event Action<string> OnRegionClick;
 
+    [SerializeField] private Animator regionAnimator;
     [SerializeField] private InfoWindow infoWindow;
     [SerializeField] private string regionNameKey;
-
+    public Animator RegionAnimator
+    {
+        get
+        {
+            if (regionAnimator == null)
+                regionAnimator = GetComponent<Animator>();
+            return regionAnimator;
+        }
+        private set { regionAnimator = value; }
+    }
     #region Public Methods
 
     public void OnPointerClick(PointerEventData eventData)
@@ -27,17 +36,34 @@ public class Region : MonoBehaviour, IPointerClickHandler
             throw new Exception("Region name missing!");
         }
     }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        RegionAnimator.SetBool("RegionShow", true);
+    }
 
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        RegionAnimator.SetBool("RegionShow", false);
+    }
+
+    //public void SetRegionShowAnimator(string val)
+    //{
+    //    RegionAnimator.SetBool("RegionShow", bool.Parse(val));
+    //}
     #endregion Public Methods
 
     private void Awake()
     {
         infoWindow = GameObject.FindGameObjectWithTag("InfoPanel").GetComponent<InfoWindow>();
         OnRegionClick += infoWindow.OnRegionClickCallback;
+        var col = gameObject.AddComponent<MeshCollider>();
+        col.sharedMesh = gameObject.GetComponent<MeshFilter>().mesh;
     }
 
     private void OnDestroy()
     {
         OnRegionClick -= infoWindow.OnRegionClickCallback;
     }
+
+    
 }
