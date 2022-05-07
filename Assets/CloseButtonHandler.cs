464589f1fc9f0b78ee85a7412d7d2111b0b20774
RunningStone1337 <runningstone@yandex.ru>
2022-05-07 10:34:53 +0300

    optimizes build size, btn close added

---
+++ Assets/CloseButtonHandler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseButtonHandler : MonoBehaviour
+{
+    public void CloseApp()
+    {
+        Application.Quit();
+    }
+}
